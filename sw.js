9449316f6903821f0f822b40b7d3d7ccd3462d03
Pembe2 <markdpemberton@gmail.com>
2026-08-12 15:12:20 +0200

    Cache job definitions for development preview

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const scopeKey = new URL(self.registration.scope).pathname.replace(/[^a-z0-9]+/gi, "-") || "root";
 const CACHE_PREFIX = `nbr-code-reference-${scopeKey}-`;
-const CACHE = `${CACHE_PREFIX}v3`;
+const CACHE = `${CACHE_PREFIX}v4`;
 const ASSETS = [
   "./",
   "index.html",
@@ -12,6 +12,7 @@ const ASSETS = [
   "data/codes-3.js",
   "data/codes-4.js",
   "data/codes-5.js",
+  "data/jobs.js",
   "manifest.json"
 ].map(path => new URL(path, self.registration.scope).href);
 
